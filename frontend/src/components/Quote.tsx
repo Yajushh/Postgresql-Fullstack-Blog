d145e8f9180ae6fba54383317ca126b5576b841c
Yajush Mishra <yajushmishra8@gmail.com>
2024-04-27 09:15:02 +0530

    update

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -4,8 +4,8 @@ export const Quote = () => {
       <div className="flex justify-center">
         <div className="max-w-lg">
           <div className=" text-3xl font-bold">
-            "The customer service was exceptionally horrible, I am in debt and
-            my wife took away the kids"
+            "Yo Yo Yo 1-4-8 3 to the 3 to the 6 to the 9. Representing the ABQ.
+            What up BIATCH! Leave at the tone."
           </div>
           <div className="max-w-md  text-xl font-semibold text-left mt-4">
             Julius Ceaser
